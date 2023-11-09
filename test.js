35b7332f6dacf498b9510a86bb33191dd35f52ae
Cowelist <Henry8bombomdog@gmail.com>
2023-11-10 00:13:09 +0100

    w

--- test.js
+++ test.js
@@ -1,34 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const lifeCounterElement = document.getElementById("life-counter");
-    const decreaseLifeButton = document.querySelectorAll('decrease-life');
-    const increaseLifeButton = document.getElementById("increase-life");
-  
-    let lifeCount = 3; // Initial life count
-  
-    // Function to update the life count display
-    function updateLifeCount() {
-      lifeCounterElement.textContent = lifeCount;
-    }
-  
-    // Function to decrease life
-    function decreaseLife() {
-      if (lifeCount > 0) {
-        lifeCount--;
-        updateLifeCount();
-      }
-    }
-  
-    // Function to increase life
-    function increaseLife() {
-      lifeCount++;
-      updateLifeCount();
-    }
-  
-    // Event listeners for the buttons
-    decreaseLifeButton.addEventListener("click", decreaseLife);
-    increaseLifeButton.addEventListener("click", increaseLife);
-  
-    // Initialize the life count display
-    updateLifeCount();
-  });
-  
+const mainTitle = document.querySelector('#life-counter');
+let curValue = 3;
+
+const decrease = document.getElementsByClassName ("textAnswer");
+const reset = document.getElementsByClassName ("reset");
+
+decrease.addEventListener('click', () => {
+  curValue++;
+  mainTitle.textContent = curValue;
+});
+
+reset.addEventListener('click', () => {
+  curValue = 3;
+  mainTitle.textContent = curValue;
+});
