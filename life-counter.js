1a3f401c18da7c4d8ffe8c2660091c5ed3d301d1
Cowelist <Henry8bombomdog@gmail.com>
2023-11-10 12:56:27 +0100

    p

--- life-counter.js
+++ life-counter.js
@@ -1,17 +1,29 @@
 let life = localStorage.getItem("life")
-
 const knapp = document.getElementById("lose")
+let spm = knapp.getAttribute("data-spm")
+
+document.getElementById ("life").textContent=life
+
+
 
 knapp.addEventListener("click",function(){
-  life -=1
- 
+  life -=1;
+  document.getElementById ("life").textContent=life
   if (life == 0){
     window.location.href="lose.html"
+    
 
 
   }
-  else
+  else{
+  /*localStorage.setItem("life", life);
+  updateLifeCounter (); */
+  window.location.href = "spm" + spm + ".html";
+  }});
 
-
-
-  })
+  /*function updateLifeCounter () {
+    const lifecounterElement = document.getElementById("life-counter");
+    if (lifeCounterElement) {
+      lifeCounterElement.textContent = "lives: " + life;
+     }
+  }*/
